d980fdafff513b780fb76882b27d3832c2c83000
nitin-421 <127936777+nitin-421@users.noreply.github.com>
2025-04-05 02:10:18 +0530

    node-1

--- Backend/index.js
+++ Backend/index.js
@@ -10,18 +10,18 @@ const server = express();
 
 main().catch(err => console.log(err, "Error in DB connection"));
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/SampleDatabase');
+  await mongoose.connect(process.env.MONGODB_URL);
   console.log('Connected to DB');
 }
 
 server.use(cors())
   .use(express.json())
   .use(morgan('combined'))
-  .use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+  // .use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
   .use('/products', productRouter.router)
-  .use('*', (_, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  })
+  // .use('*', (_, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  // })
   .listen(process.env.PORT, () => {
     console.log('Server Started on port', process.env.PORT || 2000);
   });
