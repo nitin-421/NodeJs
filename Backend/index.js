926d44905572017b533a084713998930ce348f12
nitin-421 <127936777+nitin-421@users.noreply.github.com>
2025-04-04 23:06:48 +0530

    node-1

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,27 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv/config');
+const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
-const server = express();
 const cors = require('cors');
+const path = require('path');
 const productRouter = require('./RoutersFolder/product.js');
 
-main().catch(err => console.log(err,"Error in DB connection"));
+const server = express();
+
+main().catch(err => console.log(err, "Error in DB connection"));
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URL)
+  await mongoose.connect('mongodb://localhost:27017/SampleDatabase');
   console.log('Connected to DB');
 }
 
-server.use(cors());
-server.use(express.json());
-server.use(morgan('combined'))
-server.use('/products',productRouter.router)
-
-
-server.listen(process.env.PORT, ()=>{
-	console.log('Server Started 2000');
-})
-
-// sequence of middleware is important
+server.use(cors())
+  .use(express.json())
+  .use(morgan('combined'))
+  .use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+  .use('/products', productRouter.router)
+  .use('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  })
+  .listen(process.env.PORT, () => {
+    console.log('Server Started on port', process.env.PORT || 2000);
+  });
